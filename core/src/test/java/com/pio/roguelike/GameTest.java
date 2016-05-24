af2cf8445cf2c9aa10a7817fc7679f8b8b3aa28d
Arvamer <arvamer@gmail.com>
2016-05-24 14:44:47 +0200

    Dodano szkielet do unit testów

---
+++ core/src/test/java/com/pio/roguelike/GameTest.java
@@ -0,0 +1,14 @@
+package com.pio.roguelike;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by arvamer on 5/24/16.
+ */
+public class GameTest {
+    @org.junit.Test
+    public void update() throws Exception {
+
+    }
+
+}
